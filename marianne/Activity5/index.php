fdc50fb2c70d87b1856293e91fa8fbda0cff4c3f
MedicMedic <marianneedic.13@gmail.com>
2024-03-05 09:49:25 +0800

    Activity5

---
+++ marianne/Activity5/index.php
@@ -0,0 +1,38 @@
+<html>
+   <head>
+         <title>3-BSCS-1</title>
+         <link rel="stylesheet" href="css/bootstrap.min.css">
+         <link rel="stylesheet" href="bootstrap-icons/font/bootstrap-icons.css">
+         <script src="js/jquery.js"></script>
+         <script src="js/bootstrap.bundle.min.js"></script>
+    </head>
+
+   <body>
+      <h1>Activity 5</h1>
+
+      
+
+      <hr/>
+      <br/>
+
+      <div class="container">
+         <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">Open Modal Box</button>
+         <div class="modal" id="myModal">
+            <div class="modal-dialog">
+               <div class="modal-content">
+                  <div class="modal-header">
+                     <h4 class="modal-title">This is my modal header</h4>
+                     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                  </div>
+                  <div class="modal-body">
+                     <p>This is modal content</p>
+                  </div>
+                  <div class="modal-footer">
+                     <button type="button" class="btn btn-info" data-bs-dismiss="modal">Close</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
